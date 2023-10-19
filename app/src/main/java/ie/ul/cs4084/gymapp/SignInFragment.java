b6ce4f835858338bc3692279edf69bfab95b7988
Jacob' 'Beck <102110500+ShaveYourEyebrows@users.noreply.github.com>
2023-10-19 16:56:36 +0100

    Added FireBase authentification
    Put in FireBase functionality with an email sign in.

--- app/src/main/java/ie/ul/cs4084/gymapp/SignInFragment.java
+++ app/src/main/java/ie/ul/cs4084/gymapp/SignInFragment.java
@@ -1,12 +1,26 @@
 package com.example.cs4084_project;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,12 +29,11 @@ import android.view.ViewGroup;
  */
 public class SignInFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    int signIn = 19;
+    Button sign;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +41,7 @@ public class SignInFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignInFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static SignInFragment newInstance(String param1, String param2) {
         SignInFragment fragment = new SignInFragment();
         Bundle args = new Bundle();
@@ -56,9 +61,50 @@ public class SignInFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sign = view.findViewById(R.id.signinplease);
+
+        sign.setOnClickListener(this::OnClickSignIn);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+
+    public void OnClickSignIn(View view) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), signIn
+        );
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == signIn){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == Activity.RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                System.out.println("Hello, " +user.getDisplayName() +
+                        "!\nemail: " + user.getEmail() +
+                        "\nid: " + user.getUid());
+            }else{
+                if(response == null){
+                    System.out.println("Sign in failed");
+                    return;
+                }
+                if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
+                    System.out.println("You appear to be offline");
+                    return;
+                }
+            }
+        }
     }
 }
