8d06b7128febb3936deaeffae7792fb1d2c815ee
Italo <92447706+italotrt@users.noreply.github.com>
2023-11-07 14:07:47 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "ie.ul.cs4084.gymapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ie.ul.cs4084.gymapp"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -29,14 +30,21 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
 
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    val nav_version = "2.3.0-alpha01";
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
