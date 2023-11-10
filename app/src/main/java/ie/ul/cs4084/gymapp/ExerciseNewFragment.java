5658e0e23bc4e09618dd2cb1e29c6abea3dc92ef
Italo <92447706+italotrt@users.noreply.github.com>
2023-11-10 16:27:11 +0000

    Exercise planner user input

--- app/src/main/java/ie/ul/cs4084/gymapp/ExerciseNewFragment.java
+++ app/src/main/java/ie/ul/cs4084/gymapp/ExerciseNewFragment.java
@@ -1,15 +1,19 @@
 package ie.ul.cs4084.gymapp;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +21,14 @@ import android.widget.Button;
  * create an instance of this fragment.
  */
 public class ExerciseNewFragment extends Fragment {
+    private TextInputLayout textInputExerciseName;
+    private TextInputLayout textInputBodyPart;
+    private TextInputLayout numberInputWeight;
+    private TextInputLayout numberInputSets;
+    private TextInputLayout numberInputReps;
+    private FirebaseFirestore db;
+
+    private CollectionReference exercises;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -69,12 +81,92 @@ public class ExerciseNewFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        textInputExerciseName = view.findViewById(R.id.text_input_exercise_name);
+        textInputBodyPart = view.findViewById(R.id.text_input_body_part);
+        numberInputWeight = view.findViewById(R.id.number_input_weight);
+        numberInputSets = view.findViewById(R.id.number_input_sets);
+        numberInputReps = view.findViewById(R.id.number_input_reps);
+        db = FirebaseFirestore.getInstance();
+        exercises = db.collection("Exercises");
+
         Button addExercise = view.findViewById(R.id.btnAddNewExercise);
         addExercise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean validation = validateExerciseName() && validateBodyPart() && validateWeight() && validateSets() && validateReps();
 
+                if(validation) {
+                    Map<String, Object> exercise1 = new HashMap<>();
+                    exercise1.put("Name", textInputExerciseName);
+                    exercise1.put("Body Part", textInputBodyPart);
+                    exercise1.put("Weight", numberInputWeight);
+                    exercise1.put("Sets", numberInputSets);
+                    exercise1.put("Reps", numberInputReps);
+                    exercises.document(textInputExerciseName.getEditText().getText().toString()).set(exercise1);
+
+                    getActivity().onBackPressed();
+                }
             }
         });
+    }
+
+    private boolean validateExerciseName() {
+        String exerciseNameInput = textInputExerciseName.getEditText().getText().toString();
+
+        if (exerciseNameInput.isEmpty()) {
+            textInputExerciseName.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputExerciseName.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateBodyPart() {
+        String bodyPartInput = textInputBodyPart.getEditText().getText().toString();
+
+        if (bodyPartInput.isEmpty()) {
+            textInputBodyPart.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputBodyPart.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateWeight() {
+        String weightInput = numberInputWeight.getEditText().getText().toString();
+
+        if (weightInput.isEmpty()) {
+            numberInputWeight.setError("Field can't be empty");
+            return false;
+        } else {
+            numberInputWeight.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateSets() {
+        String setsInput = numberInputSets.getEditText().getText().toString();
+
+        if (setsInput.isEmpty()) {
+            numberInputSets.setError("Field can't be empty");
+            return false;
+        } else {
+            numberInputSets.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateReps() {
+        String repsInput = numberInputReps.getEditText().getText().toString();
+
+        if (repsInput.isEmpty()) {
+            numberInputReps.setError("Field can't be empty");
+            return false;
+        } else {
+            numberInputReps.setError(null);
+            return true;
+        }
     }
 }
