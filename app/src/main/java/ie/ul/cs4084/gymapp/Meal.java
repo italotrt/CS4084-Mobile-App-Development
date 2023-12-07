98b73cbc6b3a322c9741d1ce6796f24e3fa9d2c8
Aaron <116449086+Aaron9703@users.noreply.github.com>
2023-12-07 19:43:32 +0000

    Update Meal.java
    Added methods for handling parcels, which is used for passing the meal list data to a List activity so it can be displayed.
    
    Added an overloaded constructor for initialising ingredients.

--- app/src/main/java/ie/ul/cs4084/gymapp/Meal.java
+++ app/src/main/java/ie/ul/cs4084/gymapp/Meal.java
@@ -1,9 +1,12 @@
 package com.example.cs4084_project;
 
 import java.util.ArrayList;
+import android.os.Parcel;
+import android.os.Parcelable;
+import androidx.annotation.NonNull;
 
 //A class for a Meal that will be used in the Meal Planner
-public class Meal {
+public class Meal implements Parcelable {
     String name;//Name of Meal
     int calories; //Number of calories in the Meal
     ArrayList<String> ingredients; //An ArrayList of Strings storing the ingredients used
@@ -14,6 +17,30 @@ public class Meal {
         this.calories = calories;
     }
 
+    public Meal(String name, int calories, ArrayList<String> ingredients) {
+        this.name = name;
+        this.calories = calories;
+        this.ingredients = ingredients;
+    }
+
+    protected Meal(Parcel in) {
+        name = in.readString();
+        calories = in.readInt();
+        ingredients = in.createStringArrayList();
+    }
+
+    public static final Creator<Meal> CREATOR = new Creator<Meal>() {
+        @Override
+        public Meal createFromParcel(Parcel in) {
+            return new Meal(in);
+        }
+
+        @Override
+        public Meal[] newArray(int size) {
+            return new Meal[size];
+        }
+    };
+    
     //Getter for the name variable (Returns String)
     public String getName() {
         return name;
@@ -41,6 +68,18 @@ public class Meal {
     //A toString method returning the Meal info
     @Override
     public String toString(){
-        return "Name: " + name + "\n Ingredients: " + getIngredients() +"\nCalories: " + calories;
+        return "Name: " + name + "\nIngredients: " + getIngredients() +"\nCalories: " + calories;
+    }
+
+     @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(calories);
+        dest.writeStringList(ingredients);
     }
 }
